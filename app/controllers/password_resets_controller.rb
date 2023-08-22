b4a177a016ae37bcaea520ed3dc0f7ca1d626fe2
rinch <rinchanruikun1650@gmail.com>
2023-08-22 18:02:19 +0900

    Fix: パスワードリセット機能の修正

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -24,12 +24,12 @@ class PasswordResetsController < ApplicationController
     @user = User.load_from_reset_password_token(params[:id])
     if @user.blank?
       not_authenticated
-      return
     end
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
       redirect_to(root_path, notice: 'パスワードが正常に更新されました')
     else
+      flash.now[:danger] = 'パスワードリセットに失敗しました'
       render :edit
     end
   end
